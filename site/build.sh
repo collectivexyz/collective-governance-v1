b4c0102d6e29b1649ef9ca58140d9f9b074a49ef
John Cairns <john@2ad.com>
2022-07-08 14:35:12 -0500

    html -> docs

--- site/build.sh
+++ site/build.sh
@@ -4,4 +4,4 @@ VERSION=$(git rev-parse HEAD | cut -c 1-10)
 PROJECT=eth-community-v1/$(basename ${PWD})
 
 docker build . -t ${PROJECT}:${VERSION} && \
-	docker run -v ${PWD}/../html:/html -p 8000:8000 --rm -i -t ${PROJECT}:${VERSION}
+	docker run -v ${PWD}/../docs:/html -p 8000:8000 --rm -i -t ${PROJECT}:${VERSION}
