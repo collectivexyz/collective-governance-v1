87799cad2045696759c57a2b94c07344ea528dde
John Cairns <john@2ad.com>
2022-09-20 09:29:41 -0500

    jac/readthedocs (#7)
    * 6: Read the Docs
    
    - add sha3sum to repo
    
    - use readthedocs theme, github pages from action
    
    - deploy from main
    
    * Remove the docs hierarchy

--- bin/update_abi.sh
+++ bin/update_abi.sh
@@ -13,4 +13,5 @@ for contract in VoteStrategy Storage Governance GovernanceBuilder VoterClassFact
 do
     echo "inspect abi: ${contract} to abi/${contract}.json"
     forge inspect contracts/${contract}.sol:${contract} abi > abi/${contract}.json
+    bin/sha3sum abi/${contract}.json
 done
--- site/conf.py
+++ site/conf.py
@@ -13,7 +13,6 @@ author = 'Collective.XYZ'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +22,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+# read the docs
+extensions = ['sphinx_rtd_theme']
+html_theme = 'sphinx_rtd_theme'
