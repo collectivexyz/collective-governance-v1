9ff9c278b23fd08eb13bbafd6a456e5b4d6b79a6
John Cairns <john@2ad.com>
2022-10-26 18:40:36 +0000

    Fix deployment script in GitHub Action

---
+++ bin/library.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" parse foundry deployment into library path statement """
+import sys
+import json
+import os
+
+def usage(name: str): 
+  """ print the usage """
+  print('%s deployment.json ...' % name)
+
+def parse_deployment(path: str, file: str) -> str:
+  """ parse a deployment file into a library path """
+  basename = os.path.basename(file)
+  (basefile, _) = os.path.splitext(basename)
+  (basecontract, _) = os.path.splitext(basefile)
+  with open(file, 'r') as json_stream:
+    jsondata = json_stream.read().rstrip().lstrip()
+    js = json.loads(jsondata)
+    if 'deployedTo' in js:
+      deployed_to = js['deployedTo']
+      return path + basefile + ':' + basecontract + ':' + deployed_to
+    else:
+      raise Exception('Invalid deployment json');
+  
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    library_path = []
+    contract_path = 'contracts/'
+    parse_contract_path = False
+    for file in sys.argv[1:]:
+      if file == '-c':
+        parse_contract_path = True
+      elif parse_contract_path:
+        contract_path = file
+      else:
+        library_path.append(parse_deployment(contract_path, file))
+    print(','.join(library_path))
+  else:
+    usage(sys.argv[0])
